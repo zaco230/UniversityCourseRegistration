a51272719279ee500ed9686f8e8592bc091c82f0
zaco230 <zaccote@hotmail.com>
2018-07-10 20:17:25 -0300

    Test Classes
    Some tests were added for RegisteredClasses and WelcomePage.

--- app/src/androidTest/java/com/example/zacco/universitycourseregistration/RegisteredClasses_Espresso.java
+++ app/src/androidTest/java/com/example/zacco/universitycourseregistration/RegisteredClasses_Espresso.java
@@ -3,21 +3,56 @@ package com.example.zacco.universitycourseregistration;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.ListView;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 
 /* TODO */
 
+
 @RunWith(AndroidJUnit4.class)
 public class RegisteredClasses_Espresso {
+    private ListView listView;
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(WelcomePage.class);
+
+    public RegisteredClasses_Espresso() {
+        listView = (ListView) listView.findViewById(R.id.courses);
+    }
+
     @Before
-    public void initValidAndInvalidString(){
+    public void initValidAndInvalidString()
+    {
         Intents.init();
+    }
+
+    @Test
+    public void studentInfoIsDisplayed() {
+        onView(withId(R.id.studentInfo)).check(matches(withId(R.id.studentInfo)));
+        onView(withId(R.id.studentID)).check(matches(withId(R.id.studentID)));
+    }
+
+    @Test
+    public void restaurantListViewPopulates() {
+        assertNotNull(listView.getAdapter());
+        assertEquals(listView.getAdapter().getCount(), listView.getAdapter().getCount());
+    }
+
+    @Test
+    public void studentClassesAreDisplayed() {
+        onView(withId(R.id.courses)).check(matches(withId(R.id.courses)));
     }
 
 
--- app/src/main/java/com/example/zacco/universitycourseregistration/RegisteredClasses.java
+++ app/src/main/java/com/example/zacco/universitycourseregistration/RegisteredClasses.java
@@ -1,14 +1,70 @@
 package com.example.zacco.universitycourseregistration;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.ListActivity;
+import android.widget.TextView;
 
-public class RegisteredClasses extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class RegisteredClasses extends ListActivity {
+
+    ListView simpleList;
+    private FirebaseAuth auth;
+    private DatabaseReference dbRef;
+
+    String currentClasses[] = {"Math", "Eng", "Science"};
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registered_classes);
+        populateRegistered();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, currentClasses);
+        ListView listView = (ListView) findViewById(R.id.courses);
+        setListAdapter(arrayAdapter);
     }
+
+    public void populateRegistered(){
+        auth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getUid()).child("Courses");
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot) {
+                for (DataSnapshot students : dataSnapshot.getChildren()){
+                    Log.v("what",""+ students.getKey());
+                    Log.v("tmz", "" + students.child("First Name").getValue());
+                    String key = students.getKey();
+                    if(key.equals(auth.getUid())){
+                        String name = students.child("First Name").getValue().toString() + " " + students.child("Last Name").getValue().toString();
+                        String uid = auth.getUid();
+                        setContentView(R.layout.activity_registered_classes);
+                        TextView textView = (TextView) findViewById(R.id.studentInfo);
+                        textView.setText(name);
+                        textView = (TextView) findViewById(R.id.studentID);
+                        textView.setText(uid);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("check", "failed");
+            }
+        });
+    }
+
 }
